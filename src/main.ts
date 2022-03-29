34d4ab42ef3e4b24edf86c77097e0d790a4c68f5
Leonid <sleonidy@gmail.com>
2022-03-29 22:27:58 +0300

    Init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,72 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.startGroup('Prepare')
+    const tag = core.getInput('tag')
+    const token = core.getInput('token')
+    const octokit = github.getOctokit(token)
+    const repository = core.getInput('repository')
+    const message = core.getInput('message') || tag
+    const sha = core.getInput('sha')
+    const qualifiedRepository =
+      repository || `${github.context.repo.owner}/${github.context.repo.repo}`
+    core.debug(`Repository ${qualifiedRepository}`)
+    const splitedRepository = qualifiedRepository.split('/')
+    if (splitedRepository.length !== 2) {
+      throw new Error(
+        "Repository should be provided in syntax of '{owner}/{repo}'"
+      )
+    }
+    const owner = splitedRepository[0]
+    const repo = splitedRepository[1]
+    const shouldDeleteTag = core.getBooleanInput('delete')
+    let foundTagSha = ''
+    core.endGroup()
+    core.startGroup('Fetch tag if exists')
+    try {
+      const getTagResponse = await octokit.rest.git.getTag({
+        owner,
+        repo,
+        tag_sha: tag
+      })
+      foundTagSha = getTagResponse.data.sha
+    } catch (e) {
+      core.info(`Didn't find tag ${tag}`)
+    }
+    core.endGroup()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (shouldDeleteTag && foundTagSha !== '') {
+      core.startGroup('Delete tag')
+      core.info(`Deleting ${tag} with sha ${foundTagSha}`)
+      const deleteResponse = await octokit.rest.git.deleteRef({
+        owner,
+        repo,
+        ref: `refs/tags/${tag}`
+      })
+      core.debug(`Delete status ${deleteResponse.status}`)
+      core.endGroup()
+    }
+    core.startGroup('Create tag')
+    const shaToCreateTag = foundTagSha || sha
+    const replaceTag = core.getInput('replace_tag')
+    const tagToCreate = replaceTag || tag
+    await octokit.rest.git.createTag({
+      owner,
+      repo,
+      tag: tagToCreate,
+      message,
+      object: shaToCreateTag,
+      type: 'commit',
+      tagger: {
+        name: github.context.actor,
+        email: `${github.context.actor}@noreply.github.com`
+      }
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    core.notice(`Created ${tag} at ${shaToCreateTag}`)
+    core.endGroup()
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
